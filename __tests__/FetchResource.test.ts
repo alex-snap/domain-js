e9918766534d1ce02f3513f9837856c2b4eb82eb
dikobrazik <ilnar.khusaenov@gmail.com>
2020-09-09 14:51:10 +0300

    fix(lib): code style

--- __tests__/FetchResource.test.ts
+++ __tests__/FetchResource.test.ts
@@ -1,24 +1,24 @@
-import { FetchResource, DefaultFetchOptions } from "../src/FetchResource";
-import "whatwg-fetch";
+import { FetchResource, DefaultFetchOptions } from '../src/FetchResource';
+import 'whatwg-fetch';
 
 // global;
 declare var global: any;
 let fetchResource: TestingFetchResource;
 const timeOffset = new Date().getTimezoneOffset() * -1;
-const baseUrl = "https://www.google.com/";
-const successResponseData = { data: "success", _status: 200 };
+const baseUrl = 'https://www.google.com/';
+const successResponseData = { data: 'success', _status: 200 };
 
 const successResponse = new Response(JSON.stringify(successResponseData), {
   status: 200,
   headers: {
-    "content-type": "application/json",
+    'content-type': 'application/json',
   },
 });
 
 const failedResponse = new Response(JSON.stringify(successResponseData), {
   status: 404,
   headers: {
-    "content-type": "application/json",
+    'content-type': 'application/json',
   },
 });
 
@@ -49,18 +49,18 @@ afterEach(() => {
   delete global.fetch;
 });
 
-describe("FetchResource options", () => {
-  it("trailing slash", async () => {
+describe('FetchResource options', () => {
+  it('trailing slash', async () => {
     try {
       const testFetchResource = new TestingFetchResource(baseUrl, {
         trailingSlash: false,
       });
       const expectedUrl = expect.stringContaining(`${baseUrl}test_url`);
-      const response = await testFetchResource.post("test_url", { data: 1 });
+      const response = await testFetchResource.post('test_url', { data: 1 });
       expect(global.fetch).toHaveBeenCalledWith(
         expectedUrl,
         expect.objectContaining({
-          method: "post",
+          method: 'post',
           body: JSON.stringify({ data: 1 }),
         })
       );
@@ -72,32 +72,32 @@ describe("FetchResource options", () => {
   });
 });
 
-describe("FetchResource instance", () => {
-  it("fetchResource instance of FetchResource", async () => {
+describe('FetchResource instance', () => {
+  it('fetchResource instance of FetchResource', async () => {
     expect(fetchResource).toBeInstanceOf(FetchResource);
   });
 
-  it("should store base url", () => {
+  it('should store base url', () => {
     expect(fetchResource._getBaseUrl()).toBe(baseUrl);
   });
 
-  it("should contain default options", () => {
+  it('should contain default options', () => {
     expect(fetchResource._getDefaultOptions()).toEqual(DefaultFetchOptions);
   });
 });
 
-describe("Post request", () => {
-  it("method should be defined", () => {
+describe('Post request', () => {
+  it('method should be defined', () => {
     expect(fetchResource.post).toBeInstanceOf(Function);
   });
-  it("should receive success response", async () => {
+  it('should receive success response', async () => {
     try {
       const expectedUrl = expect.stringContaining(`${baseUrl}test_url/`);
-      const response = await fetchResource.post("test_url", { data: 1 });
+      const response = await fetchResource.post('test_url', { data: 1 });
       expect(global.fetch).toHaveBeenCalledWith(
         expectedUrl,
         expect.objectContaining({
-          method: "post",
+          method: 'post',
           body: JSON.stringify({ data: 1 }),
         })
       );
@@ -107,10 +107,10 @@ describe("Post request", () => {
       expect(error).toBeNull();
     }
   });
-  it("should catch error response", async () => {
+  it('should catch error response', async () => {
     try {
-      await fetchResource.post("test_url", { data: "1" });
-      await fetchResource.post("test_url", { data: "1" });
+      await fetchResource.post('test_url', { data: '1' });
+      await fetchResource.post('test_url', { data: '1' });
     } catch (error) {
       expect(error).toBeDefined();
       expect(error.status).toEqual(404);
@@ -118,18 +118,18 @@ describe("Post request", () => {
   });
 });
 
-describe("Put request", () => {
-  it("method should be defined", () => {
+describe('Put request', () => {
+  it('method should be defined', () => {
     expect(fetchResource.put).toBeInstanceOf(Function);
   });
-  it("should receive success response", async () => {
+  it('should receive success response', async () => {
     try {
       const expectedUrl = expect.stringContaining(`${baseUrl}test_url/`);
-      const response = await fetchResource.put("test_url", { data: 1 });
+      const response = await fetchResource.put('test_url', { data: 1 });
       expect(global.fetch).toHaveBeenCalledWith(
         expectedUrl,
         expect.objectContaining({
-          method: "put",
+          method: 'put',
           body: JSON.stringify({ data: 1 }),
         })
       );
@@ -139,10 +139,10 @@ describe("Put request", () => {
       expect(error).toBeNull();
     }
   });
-  it("should catch error response", async () => {
+  it('should catch error response', async () => {
     try {
-      await fetchResource.put("test_url", { data: "1" });
-      await fetchResource.put("test_url", { data: "1" });
+      await fetchResource.put('test_url', { data: '1' });
+      await fetchResource.put('test_url', { data: '1' });
     } catch (error) {
       expect(error).toBeDefined();
       expect(error.status).toEqual(404);
@@ -150,18 +150,18 @@ describe("Put request", () => {
   });
 });
 
-describe("Patch request", () => {
-  it("method should be defined", () => {
+describe('Patch request', () => {
+  it('method should be defined', () => {
     expect(fetchResource.patch).toBeInstanceOf(Function);
   });
-  it("should receive success response", async () => {
+  it('should receive success response', async () => {
     try {
       const expectedUrl = expect.stringContaining(`${baseUrl}test_url/`);
-      const response = await fetchResource.patch("test_url", { data: 1 });
+      const response = await fetchResource.patch('test_url', { data: 1 });
       expect(global.fetch).toHaveBeenCalledWith(
         expectedUrl,
         expect.objectContaining({
-          method: "patch",
+          method: 'patch',
           body: JSON.stringify({ data: 1 }),
         })
       );
@@ -171,10 +171,10 @@ describe("Patch request", () => {
       expect(error).toBeNull();
     }
   });
-  it("should catch error response", async () => {
+  it('should catch error response', async () => {
     try {
-      await fetchResource.patch("test_url", { data: "1" });
-      await fetchResource.patch("test_url", { data: "1" });
+      await fetchResource.patch('test_url', { data: '1' });
+      await fetchResource.patch('test_url', { data: '1' });
     } catch (error) {
       expect(error).toBeDefined();
       expect(error.status).toEqual(404);
@@ -182,17 +182,17 @@ describe("Patch request", () => {
   });
 });
 
-describe("Get request", () => {
-  it("method should be defined", () => {
+describe('Get request', () => {
+  it('method should be defined', () => {
     expect(fetchResource.get).toBeInstanceOf(Function);
   });
-  it("should receive success response", async () => {
+  it('should receive success response', async () => {
     try {
       const expectedUrl = expect.stringContaining(`${baseUrl}test_url/`);
-      const response = await fetchResource.get("test_url");
+      const response = await fetchResource.get('test_url');
       expect(global.fetch).toHaveBeenCalledWith(
         expectedUrl,
-        expect.objectContaining({ method: "get" })
+        expect.objectContaining({ method: 'get' })
       );
       expect(response).toBeDefined();
       expect(response).toEqual(successResponseData);
@@ -200,57 +200,57 @@ describe("Get request", () => {
       expect(error).toBeNull();
     }
   });
-  it("should catch error response", async () => {
+  it('should catch error response', async () => {
     try {
-      await fetchResource.get("test_url");
-      await fetchResource.get("test_url");
+      await fetchResource.get('test_url');
+      await fetchResource.get('test_url');
     } catch (error) {
       expect(error).toBeDefined();
       expect(error.status).toEqual(404);
     }
   });
-  it("should pass query params in request", async () => {
+  it('should pass query params in request', async () => {
     const queryString = `${baseUrl}test_url/?page=1&per_page=10&array=1,2,3&timeoffset=${timeOffset}`;
     const expectedUrl = expect.stringContaining(queryString);
-    await fetchResource.get("test_url", {
+    await fetchResource.get('test_url', {
       page: 1,
       per_page: 10,
       array: [1, 2, 3],
     });
     expect(global.fetch).toHaveBeenCalledWith(
       expectedUrl,
-      expect.objectContaining({ method: "get" })
+      expect.objectContaining({ method: 'get' })
     );
   });
-  it("should pass query params array as array in request", async () => {
+  it('should pass query params array as array in request', async () => {
     const queryTestfetchResource = new TestingFetchResource(baseUrl, {
-      queryParamsDecodeMode: "array",
+      queryParamsDecodeMode: 'array',
     });
     const queryString = `${baseUrl}test_url/?page=1&per_page=10&array[]=1&array[]=2&array[]=3&timeoffset=${timeOffset}`;
     const expectedUrl = expect.stringContaining(queryString);
-    await queryTestfetchResource.get("test_url", {
+    await queryTestfetchResource.get('test_url', {
       page: 1,
       per_page: 10,
       array: [1, 2, 3],
     });
     expect(global.fetch).toHaveBeenCalledWith(
       expectedUrl,
-      expect.objectContaining({ method: "get" })
+      expect.objectContaining({ method: 'get' })
     );
   });
 });
 
-describe("Delete request", () => {
-  it("method should be defined", () => {
+describe('Delete request', () => {
+  it('method should be defined', () => {
     expect(fetchResource.delete).toBeInstanceOf(Function);
   });
-  it("should receive success response", async () => {
+  it('should receive success response', async () => {
     try {
       const expectedUrl = expect.stringContaining(`${baseUrl}test_url/`);
-      const response = await fetchResource.delete("test_url");
+      const response = await fetchResource.delete('test_url');
       expect(global.fetch).toHaveBeenCalledWith(
         expectedUrl,
-        expect.objectContaining({ method: "delete" })
+        expect.objectContaining({ method: 'delete' })
       );
       expect(response).toBeDefined();
       expect(response).toEqual(successResponseData);
@@ -258,10 +258,10 @@ describe("Delete request", () => {
       expect(error).toBeNull();
     }
   });
-  it("should catch error response", async () => {
+  it('should catch error response', async () => {
     try {
-      await fetchResource.delete("test_url");
-      await fetchResource.delete("test_url");
+      await fetchResource.delete('test_url');
+      await fetchResource.delete('test_url');
     } catch (error) {
       expect(error).toBeDefined();
       expect(error.status).toEqual(404);
@@ -269,23 +269,23 @@ describe("Delete request", () => {
   });
 });
 
-describe("Set headers", () => {
-  it("method should be defined", () => {
+describe('Set headers', () => {
+  it('method should be defined', () => {
     expect(fetchResource.setHeaders).toBeInstanceOf(Function);
   });
 
-  it("should store headers", () => {
-    const headers = { Authorization: "X" };
+  it('should store headers', () => {
+    const headers = { Authorization: 'X' };
     fetchResource.setHeaders(headers);
     const savedHeaders = fetchResource._getDefaultOptions().headers;
     expect(savedHeaders).toEqual(headers);
   });
 
-  it("should send headers on each request", async () => {
-    const headers = { Authorization: "X" };
+  it('should send headers on each request', async () => {
+    const headers = { Authorization: 'X' };
     fetchResource.setHeaders(headers);
     const expectedUrl = expect.stringContaining(`${baseUrl}test_url/`);
-    await fetchResource.get("test_url");
+    await fetchResource.get('test_url');
     expect(global.fetch).toHaveBeenCalledWith(
       expectedUrl,
       expect.objectContaining({ headers: expect.objectContaining(headers) })
@@ -293,12 +293,12 @@ describe("Set headers", () => {
   });
 });
 
-describe("Clear headers", () => {
-  it("method should be defined", () => {
+describe('Clear headers', () => {
+  it('method should be defined', () => {
     expect(fetchResource.clearHeaders).toBeInstanceOf(Function);
   });
-  it("should clear headers", () => {
-    const headers = { Authorization: "X" };
+  it('should clear headers', () => {
+    const headers = { Authorization: 'X' };
     fetchResource.setHeaders(headers);
     fetchResource.clearHeaders();
     const savedHeaders = fetchResource._getDefaultOptions().headers;
@@ -306,63 +306,60 @@ describe("Clear headers", () => {
   });
 });
 
-describe("Set base path dynamically", () => {
-  it("method should be defined", () => {
+describe('Set base path dynamically', () => {
+  it('method should be defined', () => {
     expect(fetchResource.setBasePath).toBeInstanceOf(Function);
   });
 
-  it("should change base path", async () => {
-    const newBasePath = "https://www.newBasePath.ru/";
+  it('should change base path', async () => {
+    const newBasePath = 'https://www.newBasePath.ru/';
     const expectedUrl = expect.stringContaining(`${newBasePath}test_url`);
     fetchResource.setBasePath(newBasePath);
-    await fetchResource.get("test_url");
+    await fetchResource.get('test_url');
     expect(global.fetch).toHaveBeenCalledWith(expectedUrl, expect.any(Object));
   });
 });
 
-describe("Resolve destination", () => {
-  it("method should be defined", () => {
+describe('Resolve destination', () => {
+  it('method should be defined', () => {
     expect(fetchResource.resolveDestination).toBeInstanceOf(Function);
   });
 
-  it("should create path from parts", () => {
-    const relativePath = "users";
-    const additionalParts = ["part1", "part2", "part3"];
-    const path = fetchResource.resolveDestination(
-      additionalParts,
-      relativePath
-    );
-    expect(path).toEqual("users/part1/part2/part3");
+  it('should create path from parts', () => {
+    const relativePath = 'users';
+    const additionalParts = ['part1', 'part2', 'part3'];
+    const path = fetchResource.resolveDestination(additionalParts, relativePath);
+    expect(path).toEqual('users/part1/part2/part3');
   });
 });
 
-describe("Get query string", () => {
-  it("method should be defined", () => {
+describe('Get query string', () => {
+  it('method should be defined', () => {
     expect(fetchResource.getQueryString).toBeInstanceOf(Function);
   });
 
-  it("should create query string from object", () => {
+  it('should create query string from object', () => {
     const result1 = fetchResource.getQueryString({
-      coupons: ["SL-6TXY0-QB524OT"],
+      coupons: ['SL-6TXY0-QB524OT'],
     });
     const result2 = fetchResource.getQueryString(
-      { coupons: ["SL-6TXY0-QB524OT"] },
-      { queryParamsDecodeMode: "array" }
+      { coupons: ['SL-6TXY0-QB524OT'] },
+      { queryParamsDecodeMode: 'array' }
     );
     const result3 = fetchResource.getQueryString(
-      { coupons: ["SL-6TXY0-QB524OT"] },
+      { coupons: ['SL-6TXY0-QB524OT'] },
       {
-        queryParamsDecodeMode: "array",
+        queryParamsDecodeMode: 'array',
         timeOffset: false,
       }
     );
     const result4 = fetchResource.getQueryString(
       { coupons: [] },
-      { queryParamsDecodeMode: "array" }
+      { queryParamsDecodeMode: 'array' }
     );
-    expect(result1).toEqual("coupons=SL-6TXY0-QB524OT&timeoffset=180");
-    expect(result2).toEqual("coupons[]=SL-6TXY0-QB524OT&timeoffset=180");
-    expect(result3).toEqual("coupons[]=SL-6TXY0-QB524OT");
-    expect(result4).toEqual("timeoffset=180");
+    expect(result1).toEqual('coupons=SL-6TXY0-QB524OT&timeoffset=180');
+    expect(result2).toEqual('coupons[]=SL-6TXY0-QB524OT&timeoffset=180');
+    expect(result3).toEqual('coupons[]=SL-6TXY0-QB524OT');
+    expect(result4).toEqual('timeoffset=180');
   });
 });
--- src/FetchResource.ts
+++ src/FetchResource.ts
@@ -1,8 +1,8 @@
-import { BaseResource } from "./interfaces/BaseResource";
-import { ContentTypes } from "./enums/ContentTypes";
-import { isFunction } from "./helpers";
+import { BaseResource } from './interfaces/BaseResource';
+import { ContentTypes } from './enums/ContentTypes';
+import { isFunction } from './helpers';
 
-export type FetchRequestMethod = "post" | "put" | "get" | "delete" | "patch";
+export type FetchRequestMethod = 'post' | 'put' | 'get' | 'delete' | 'patch';
 
 export interface FetchOptions {
   isFormData?: boolean;
@@ -15,10 +15,10 @@ export interface FetchOptions {
   cache?: RequestCache;
   credentials?: RequestCredentials;
   redirect?: RequestRedirect;
-  referrer?: "no-referrer" | "client";
+  referrer?: 'no-referrer' | 'client';
   timeOffset?: boolean;
   handleError?: (payload: { response: Response; parsedBody: any }) => any;
-  queryParamsDecodeMode?: "comma" | "array";
+  queryParamsDecodeMode?: 'comma' | 'array';
   queryParams?: any;
   // params?: any
   // todo
@@ -41,17 +41,17 @@ export interface FetchRequestOptions {
 export const DefaultFetchOptions: FetchOptions = {
   headers: {},
   trailingSlash: true,
-  responseType: "json",
+  responseType: 'json',
   contentType: ContentTypes.JSON,
-  accessType: "json",
-  mode: "same-origin",
-  cache: "default",
-  credentials: "same-origin",
-  redirect: "follow",
-  referrer: "client",
+  accessType: 'json',
+  mode: 'same-origin',
+  cache: 'default',
+  credentials: 'same-origin',
+  redirect: 'follow',
+  referrer: 'client',
   timeOffset: true,
   handleError: undefined,
-  queryParamsDecodeMode: "comma",
+  queryParamsDecodeMode: 'comma',
 };
 
 export class FetchResource implements BaseResource {
@@ -66,46 +66,23 @@ export class FetchResource implements BaseResource {
   }
 
   public post(url: string, body: any, options?: FetchOptions): Promise<object> {
-    const { requestUrl, requestOptions } = this.resolveRequestData(
-      "post",
-      url,
-      options,
-      body
-    );
+    const { requestUrl, requestOptions } = this.resolveRequestData('post', url, options, body);
     return this.fetchHandleCode(requestUrl, requestOptions);
   }
 
   public put(url: string, body: any, options?: FetchOptions): Promise<object> {
-    const { requestUrl, requestOptions } = this.resolveRequestData(
-      "put",
-      url,
-      options,
-      body
-    );
+    const { requestUrl, requestOptions } = this.resolveRequestData('put', url, options, body);
     return this.fetchHandleCode(requestUrl, requestOptions);
   }
 
-  public patch(
-    url: string,
-    body: any,
-    options?: FetchOptions
-  ): Promise<object> {
-    const { requestUrl, requestOptions } = this.resolveRequestData(
-      "patch",
-      url,
-      options,
-      body
-    );
+  public patch(url: string, body: any, options?: FetchOptions): Promise<object> {
+    const { requestUrl, requestOptions } = this.resolveRequestData('patch', url, options, body);
     return this.fetchHandleCode(requestUrl, requestOptions);
   }
 
-  public get(
-    url: string,
-    queryParams?: any,
-    options?: FetchOptions
-  ): Promise<object> {
+  public get(url: string, queryParams?: any, options?: FetchOptions): Promise<object> {
     const { requestUrl, requestOptions } = this.resolveRequestData(
-      "get",
+      'get',
       url,
       options,
       queryParams
@@ -113,26 +90,13 @@ export class FetchResource implements BaseResource {
     return this.fetchHandleCode(requestUrl, requestOptions);
   }
 
-  public delete(
-    url: string,
-    body?: any,
-    options?: FetchOptions
-  ): Promise<void> {
-    const { requestUrl, requestOptions } = this.resolveRequestData(
-      "delete",
-      url,
-      options,
-      body
-    );
+  public delete(url: string, body?: any, options?: FetchOptions): Promise<void> {
+    const { requestUrl, requestOptions } = this.resolveRequestData('delete', url, options, body);
     return this.fetchHandleCode(requestUrl, requestOptions);
   }
 
   public setHeaders(headers: object) {
-    this.defaultOptions.headers = Object.assign(
-      {},
-      this.defaultOptions.headers,
-      headers
-    );
+    this.defaultOptions.headers = Object.assign({}, this.defaultOptions.headers, headers);
   }
 
   public clearHeaders() {
@@ -143,50 +107,42 @@ export class FetchResource implements BaseResource {
     this.baseUrl = baseUrl;
   }
 
-  public resolveDestination(
-    pathParts: Array<number | string>,
-    basePath: string
-  ): string {
+  public resolveDestination(pathParts: Array<number | string>, basePath: string): string {
     return (pathParts.reduce(
       (resultUrl, routePart) => `${resultUrl}/${routePart}`,
       basePath
-    ) as string).replace(/([^:]\/)\/+/g, "$1");
+    ) as string).replace(/([^:]\/)\/+/g, '$1');
   }
 
   public getAllEntities(): Promise<any> {
     return new Promise((_, reject) => {
-      reject(
-        new Error("BaseHttpResource #getAllEntities(): need to provide method")
-      );
+      reject(new Error('BaseHttpResource #getAllEntities(): need to provide method'));
     });
   }
 
   private extractResponseContent(response: Response) {
-    const responseContentType = response.headers.get("content-type");
-    if (responseContentType.indexOf("application/json") > -1) {
+    const responseContentType = response.headers.get('content-type');
+    if (responseContentType.indexOf('application/json') > -1) {
       return response.json();
-    } else if (responseContentType.indexOf("application/octet-stream") > -1) {
+    } else if (responseContentType.indexOf('application/octet-stream') > -1) {
       return response.blob();
-    } else if (responseContentType.indexOf("multipart/form-data") > -1) {
+    } else if (responseContentType.indexOf('multipart/form-data') > -1) {
       return response.formData();
     } else {
       return response.text();
     }
   }
 
-  private fetchHandleCode(
-    url: string,
-    options: FetchRequestOptions
-  ): Promise<any> {
+  private fetchHandleCode(url: string, options: FetchRequestOptions): Promise<any> {
     return new Promise((resolve, reject) => {
       this.fetchClient(url, options)
         .then(async (response: Response) => {
           if (response && response.status <= 208) {
             const data = await this.extractResponseContent(response.clone());
-            if (typeof data === "string") {
+            if (typeof data === 'string') {
               resolve(data);
             } else {
-              data["_status"] = response.status;
+              data['_status'] = response.status;
               resolve(data);
             }
           } else {
@@ -203,8 +159,7 @@ export class FetchResource implements BaseResource {
 
   private async handleError(response: Response) {
     let parsedBody = await this.extractResponseContent(response.clone());
-    this.defaultOptions?.handleError &&
-      this.defaultOptions.handleError({ response, parsedBody });
+    this.defaultOptions?.handleError && this.defaultOptions.handleError({ response, parsedBody });
     return { ...response, parsedBody };
   }
 
@@ -250,7 +205,7 @@ export class FetchResource implements BaseResource {
 
   private isFile(value: any): boolean {
     // todo realize for React Native
-    return typeof value === "object"; //&& !(value instanceof File)
+    return typeof value === 'object'; //&& !(value instanceof File)
   }
 
   private resolveRequestData(
@@ -259,31 +214,23 @@ export class FetchResource implements BaseResource {
     options?: FetchOptions,
     body?: any
   ) {
-    const queryParams =
-      method === "get" ? body : options && options.queryParams;
-    const requestBody = method === "get" ? void 0 : body;
+    const queryParams = method === 'get' ? body : options && options.queryParams;
+    const requestBody = method === 'get' ? void 0 : body;
     const mergedOptions = this.resolveRequestOptions(options);
     let requestUrl = this.resolveRequestUrl(url, mergedOptions);
     const decodedBody = this.resolveRequestBody(requestBody, options);
-    const requestOptions = this.createRequestOptions(
-      method,
-      mergedOptions,
-      decodedBody
-    );
+    const requestOptions = this.createRequestOptions(method, mergedOptions, decodedBody);
     const query = this.getQueryString(queryParams, options);
-    requestUrl =
-      query != null && query != "" ? `${requestUrl}?${query}` : requestUrl;
+    requestUrl = query != null && query != '' ? `${requestUrl}?${query}` : requestUrl;
     return { requestUrl, requestOptions };
   }
 
   private resolveRequestUrl(url: string, o?: FetchOptions): string {
     if (this.baseUrl == null) {
-      throw new Error(
-        "BaseHttpResource#resolveRequestUrl: baseUrl is not defined"
-      );
+      throw new Error('BaseHttpResource#resolveRequestUrl: baseUrl is not defined');
     }
-    const urlPart = `/${url}${o.trailingSlash ? "/" : ""}`;
-    let result = (this.baseUrl + urlPart).replace(/([^:]\/)\/+/g, "$1");
+    const urlPart = `/${url}${o.trailingSlash ? '/' : ''}`;
+    let result = (this.baseUrl + urlPart).replace(/([^:]\/)\/+/g, '$1');
     return result;
   }
 
@@ -307,7 +254,7 @@ export class FetchResource implements BaseResource {
     };
 
     if (body) {
-      result["body"] = body;
+      result['body'] = body;
     }
 
     return result;
@@ -316,12 +263,12 @@ export class FetchResource implements BaseResource {
   private resolveHeaders(options: FetchOptions) {
     const additionalHeaders = {} as any;
     if (options.contentType === ContentTypes.JSON) {
-      additionalHeaders["Content-Type"] = "application/json";
+      additionalHeaders['Content-Type'] = 'application/json';
     } else if (options.contentType === ContentTypes.FORM_DATA) {
-      additionalHeaders["Content-Type"] = "multipart/form-data";
+      additionalHeaders['Content-Type'] = 'multipart/form-data';
     }
-    if (options.responseType === "json") {
-      additionalHeaders["Accept"] = "application/json";
+    if (options.responseType === 'json') {
+      additionalHeaders['Accept'] = 'application/json';
     }
 
     return { ...options.headers, ...additionalHeaders };
@@ -329,36 +276,31 @@ export class FetchResource implements BaseResource {
 
   public getQueryString(params: any = {}, o?: FetchOptions): string {
     const options =
-      o != null
-        ? { ...this.defaultOptions, ...o }
-        : (this.defaultOptions as FetchOptions);
+      o != null ? { ...this.defaultOptions, ...o } : (this.defaultOptions as FetchOptions);
 
     if (options.timeOffset) {
-      params["timeoffset"] = new Date().getTimezoneOffset() * -1;
+      params['timeoffset'] = new Date().getTimezoneOffset() * -1;
     }
 
     return Object.keys(params)
       .map((k) => {
         if (params[k] === null || params[k] === undefined) {
-          return "";
+          return '';
         } else if (Array.isArray(params[k])) {
           switch (options.queryParamsDecodeMode) {
-            case "array":
+            case 'array':
               return params[k]
-                .map(
-                  (val: any) =>
-                    `${encodeURIComponent(k)}[]=${encodeURIComponent(val)}`
-                )
-                .join("&");
-            case "comma":
+                .map((val: any) => `${encodeURIComponent(k)}[]=${encodeURIComponent(val)}`)
+                .join('&');
+            case 'comma':
             default:
-              return `${encodeURIComponent(k)}=${params[k].join(",")}`;
+              return `${encodeURIComponent(k)}=${params[k].join(',')}`;
           }
         } else {
           return `${encodeURIComponent(k)}=${encodeURIComponent(params[k])}`;
         }
       })
       .filter(Boolean)
-      .join("&");
+      .join('&');
   }
 }
