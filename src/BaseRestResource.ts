bcf87b0d1d44faf7d59350bdc9f138973f667403
Alexey Ryabov <alex.happyes@gmail.com>
2021-11-19 12:16:42 +0300

    fix(BaseRestResource): fix options

--- src/BaseRestResource.ts
+++ src/BaseRestResource.ts
@@ -1,8 +1,11 @@
 import { BaseResource } from './interfaces/BaseResource';
-import { BaseResourceOptions } from "./interfaces/BaseResourceOptions";
 import { ResourceResponse } from "./interfaces/ResourceResponse";
+import {FetchResourceOptions} from "./resources/fetch/FetchResourceOptions";
+import {AxiosResourceOptions} from "./resources/axios/AxiosResourceOptions";
 
 type Body = Record<string, any> | null;
+
+type BaseRestResourceOptions = FetchResourceOptions | AxiosResourceOptions;
 
 export class BaseRestResource {
   protected Constructor = BaseRestResource;
@@ -10,26 +13,26 @@ export class BaseRestResource {
   constructor(
     private resource: BaseResource,
     private resourceUrl: string,
-    private options?: BaseResourceOptions
+    private options?: BaseRestResourceOptions
   ) {}
 
-  public create(body: Body = null, options?: BaseResourceOptions): Promise<ResourceResponse> {
+  public create(body: Body = null, options?: BaseRestResourceOptions): Promise<ResourceResponse> {
     return this.resource.post(this.resourceUrl, body, this.createOptions(options));
   }
 
-  public update(body: Body = null, options?: BaseResourceOptions): Promise<ResourceResponse> {
+  public update(body: Body = null, options?: BaseRestResourceOptions): Promise<ResourceResponse> {
     return this.resource.put(this.resourceUrl, body, this.createOptions(options));
   }
 
-  public patch(body: Body = null, options?: BaseResourceOptions): Promise<ResourceResponse> {
+  public patch(body: Body = null, options?: BaseRestResourceOptions): Promise<ResourceResponse> {
     return this.resource.patch(this.resourceUrl, body, this.createOptions(options));
   }
 
-  public get(body?: Body, options?: BaseResourceOptions): Promise<ResourceResponse> {
+  public get(body?: Body, options?: BaseRestResourceOptions): Promise<ResourceResponse> {
     return this.resource.get(this.resourceUrl, body, this.createOptions(options));
   }
 
-  public delete(body?: Body, options?: BaseResourceOptions): Promise<ResourceResponse> {
+  public delete(body?: Body, options?: BaseRestResourceOptions): Promise<ResourceResponse> {
     return this.resource.delete(this.resourceUrl, body, this.createOptions(options));
   }
 
@@ -42,7 +45,7 @@ export class BaseRestResource {
     return this.resource;
   }
 
-  private createOptions(options: BaseResourceOptions): BaseResourceOptions {
+  private createOptions(options: BaseRestResourceOptions): BaseRestResourceOptions {
     return Object.assign({}, this.options, options);
   }
 }
